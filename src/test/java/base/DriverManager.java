6d193adbc07b98c1f163a6f475a73154a29135cb
SaravananJayabal <saravananmsd957@gamil.com>
2026-06-08 17:37:21 +0530

    Added headless execution support

--- src/test/java/base/DriverManager.java
+++ src/test/java/base/DriverManager.java
@@ -2,19 +2,25 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     private static WebDriver driver;
 
-    private DriverManager() {
-    }
-
     public static WebDriver getDriver() {
 
         if (driver == null) {
 
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(options);
+
             driver.manage().window().maximize();
         }
 
@@ -30,4 +36,3 @@ public class DriverManager {
         }
     }
 }
-
